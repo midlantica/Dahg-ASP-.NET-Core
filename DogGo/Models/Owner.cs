dd0ab636232649a95907a10d7c170b159926ab98
drewharper <druharper@gmail.com>
2020-06-22 13:59:03 -0500

    Walk.cs class setup

--- DogGo/Models/Owner.cs
+++ DogGo/Models/Owner.cs
@@ -1,19 +1,37 @@
-﻿using System;
+﻿using DogGo.Models;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace DogGo.Models
 {
+    // Data Model
     public class Owner
     {
         public int Id { get; set; }
+
+        [EmailAddress]
+        [Required]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "Hmmm... You should really add a Name...")]
+        [MaxLength(35)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(55, MinimumLength = 5, ErrorMessage = "Enter a valid Address")]
         public string Address { get; set; }
+
+        [Required]
+        [DisplayName("Neighborhood")]
         public int NeighborhoodId { get; set; }
         public Neighborhood Neighborhood { get; set; }
+
+        [Phone]
+        [DisplayName("Telephone Number")]
         public string Phone { get; set; }
-        public List<Dog> Dogs { get; set; }
     }
 }
--- DogGo/Repositories/WalkerRepository.cs
+++ DogGo/Repositories/WalkerRepository.cs
@@ -1,7 +1,10 @@
 ﻿using DogGo.Models;
 using Microsoft.Data.SqlClient;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace DogGo.Repositories
 {
